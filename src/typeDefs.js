d9dbdbcf3b796636b3aaa3e338da2a295665c627
Ali Azlan <aliazlan2002@gmail.com>
2022-04-01 19:20:59 +0500

    type Definitions

---
+++ src/typeDefs.js
@@ -0,0 +1,29 @@
+const { gql } = require('apollo-server-express');
+
+// Construct a schema, using GraphQL schema language
+export const typeDefs = gql`
+  type Query {
+    user: User,
+    posts: [Post!]!,
+    post(id: ID!): Post,
+    userPosts(id: ID!): [Post!]!,
+    myPosts: [Post!]!,
+  },
+  type Mutation{
+    signup(name: String!, email: String!, password: String!): Boolean!,
+    signin(email: String!, password: String!): String,
+    addPost(title: String, description: String!, privacy: String): Post,
+  }
+  type User{
+    id: ID,
+    name: String,
+    email: String
+  }
+  type Post{
+    id: ID,
+    user: User,
+    title: String,
+    description: String,
+    privacy: String
+  }
+`;
