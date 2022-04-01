acac82a73f2e975545435c9687472d4c48e193e7
Ali Azlan <aliazlan2002@gmail.com>
2022-04-01 19:19:56 +0500

    controller functions

---
+++ src/controllers/user.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken'
+import User from '../models/user'
+
+exports.getUser = async (token) => {
+    try{
+        if(token == '' || token === null){
+            return null;
+        }
+
+        const { id } = jwt.verify(token, process.env.SECRET_KEY)
+        const user = await User.findById(id)
+        if(!user) return null
+
+        return user
+    }catch(error){
+        console.log(error)
+        return null
+    }
+}
